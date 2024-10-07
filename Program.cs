4e7cd6ae07c4c19ac266eeab7069171552e84623
Dervalar <lisandro.mf@hotmail.com>
2024-10-07 17:48:06 -0300

    Borrador menu

--- Program.cs
+++ Program.cs
@@ -1,20 +1,63 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace ConsoleApp4
 {
     internal class Program
     {
+
+        static bool isPlaying = true; // Variable para mantener el juego en ejecución
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("PRUEBA!");
+            
 
-            Cancer();
+
+
+            int menu = int.Parse(Console.ReadLine());
+
+            do {
+
+                Console.WriteLine("lorem ipsum");
+
+                Console.WriteLine("1- Lorem");
+                Console.WriteLine("2- Lorem");
+
+
+                switch(menu)
+                {
+                    case 1: nuevaPartida();
+                        break;
+                    case 2: Exit();
+                        break;
+                    default: Console.WriteLine("Oleme el dedo");
+                        break;
+                }
+                
+
+            } while ();
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
-        static Cancer()
-        {
-            Console.WriteLine("CANCER CANCER CANCER");
-            Console.WriteLine("Penelope");
-        }
+        
     }
 }
